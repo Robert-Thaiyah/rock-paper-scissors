811968a684679685177a5cc6b97a460f183968fa
Robert-Thaiyah <bobthaiya@gmail.com>
2022-10-12 14:36:29 +0300

    version 1 done.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rock-paper-scissors
 Simple console rock-paper-scissors game. Will be updated with an UI later.
+
+Note: This code comes from stackoverflow. I have not yet grasped the basics of JS but will have to read again from the top to fully master the basics. 
+
+Source if you want to read the thread: 
+https://stackoverflow.com/questions/17976883/rock-paper-scissors-in-javascript
---
+++ script.js
@@ -0,0 +1,53 @@
+let playerChoice = prompt("What is your choice of weapon: rock, paper, or scissors?");
+    
+    //Computer Choice
+   let computerChoice = Math.ceil(Math.random() *3);
+    
+    //variables as numbers
+    if (computerChoice < 1) {
+        computerChoice = "rock";
+    } else if(1 <= computerChoice <= 2) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    
+    
+    //defining function
+    function game(playerChoice, computerChoice){
+    
+    //Checking for a tie
+    if (playerChoice === computerChoice) {
+          return "It is a tie";
+        }
+    
+        //Check for Rock
+        if (playerChoice === "rock") {
+          if (computerChoice === "scissors") {
+            return "Player Wins";
+          } else {
+            return "Computer Wins";
+          }
+        }
+        //Check for Paper
+        if (playerChoice === "paper") {
+          if (computerChoice === "scissors") {
+            return "Computer Wins";
+          } else {
+            return "Player Wins";
+          }
+        }
+        //Check for Scissors
+        if (playerChoice === "scissors") {
+          if (computerChoice === "rock") {
+            return "Computer Wins";
+          } else {
+                    return "Player Wins";
+          }
+        }
+    }
+    
+    //start the game function
+    game();
+    //print winner
+    console.log(game(playerChoice, computerChoice))
